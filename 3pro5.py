2d6cd6d1822cab6ea0ca326114257764909454f9
Arivukkarasan27 <51949129+Arivukkarasan27@users.noreply.github.com>
2019-07-08 15:32:28 +0530

    Update 3pro5.py

--- 3pro5.py
+++ 3pro5.py
@@ -1,5 +1,7 @@
-an=int(input())
-nm=list(map(int,input().split()))[:n]
-ba=nm.sort()
-middleIndex =int( (len(nm))/2)
-print(nm[middleIndex])
+import statistics
+num1=int(input())
+li1=list(map(int,input().split()))
+li1.sort()
+for i in range(num1):
+    k=statistics.median(li1)
+print(k)
