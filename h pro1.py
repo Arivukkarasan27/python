f184ca32e32505b14d4dbb43f21bf49338029661
Arivukkarasan27 <51949129+Arivukkarasan27@users.noreply.github.com>
2019-07-10 15:39:53 +0530

    Rename hpro1.py to h pro1.py

---
+++ h pro1.py
@@ -0,0 +1,11 @@
+num1=int(input())
+hk=list(map(int,input().split()[:num1]))
+count=0
+fp=[]
+for i in range(0,num+1):
+   if(hk.count(i)>1):
+      fp.append(i)
+if(len(fp)==0):
+   print("unique")
+ew=sorted(fp)
+print(' '.join(map(str,ew)))
