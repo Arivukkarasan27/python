8f61c5b13fe6e5f21c4a6f31c94296c9eaca8479
Arivukkarasan27 <51949129+Arivukkarasan27@users.noreply.github.com>
2019-07-10 15:41:35 +0530

    Update hpro1.py

--- hpro1.py
+++ hpro1.py
@@ -2,7 +2,7 @@ num1=int(input())
 hk=list(map(int,input().split()[:num1]))
 count=0
 fp=[]
-for i in range(0,num+1):
+for i in range(0,num1+1):
    if(hk.count(i)>1):
       fp.append(i)
 if(len(fp)==0):
