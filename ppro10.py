fa9503204f0251a111ded949f4b5b5b3807ae14f
Arivukkarasan27 <51949129+Arivukkarasan27@users.noreply.github.com>
2019-08-06 18:42:59 +0530

    Create ppro10.py

---
+++ ppro10.py
@@ -0,0 +1,8 @@
+ch=int(input())
+cho=[int(o) for o in input().split(" ")]
+che=0
+for p in range(cha):
+      for g in range(p):
+           if(cho[g]<cho[p]):
+                che+=cho[g]
+print(che)
